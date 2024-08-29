ffd823a8838940b0342ee52567960642153bcfe9
j8won <joowoni99@gmail.com>
2024-08-29 23:42:45 +0900

    :bug: Fix typescript indexing issue by casting singerKey to keyof typeof SINGERS for safe access

--- lib/utils/mdx.ts
+++ lib/utils/mdx.ts
@@ -1,9 +1,9 @@
-import LOCALE from '../constants/LOCALE';
 import path from 'path';
 import fs from 'fs';
 import { sync } from 'glob';
 import matter from 'gray-matter';
 import { SingerType, SongsBySinger, SongType } from '../../types/songs';
+import LOCALE from '../constants/LOCALE';
 import { SINGERS } from '../constants/SONGS';
 
 const SUPPORTED_LOCALES = Object.keys(LOCALE.INFOS);
@@ -36,12 +36,20 @@ export const getAllSongsWithSinger = async (
   locale: string
 ): Promise<SongsBySinger[]> => {
   const result: SongsBySinger[] = [];
-  const singersMap: Record<SingerType, SongType[]> = {};
+  const singersMap: Record<SingerType, SongType[]> = Object.keys(
+    SINGERS
+  ).reduce(
+    (acc, key) => {
+      acc[SINGERS[key as keyof typeof SINGERS]] = [];
+      return acc;
+    },
+    {} as Record<SingerType, SongType[]>
+  );
   const localePath = songsDirectory[locale];
   const files = sync(`${localePath}/**/*.mdx`);
 
   files.forEach((file) => {
-    const singerKey = path.basename(path.dirname(file));
+    const singerKey = path.basename(path.dirname(file)) as keyof typeof SINGERS;
     const singer = SINGERS[singerKey] || singerKey;
 
     const metadata = getSongMetadata(file);
