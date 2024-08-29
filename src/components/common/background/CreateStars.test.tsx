04f25a38dc170c32c430b733908868f3342fa920
j8won <joowoni99@gmail.com>
2024-08-29 13:48:59 +0900

    :white_check_mark: Refactor unit tests for TwinkleStars component to handle multiple star elements and validate each element's presence, class, and position correctly

--- src/components/common/background/CreateStars.test.tsx
+++ src/components/common/background/CreateStars.test.tsx
@@ -1,22 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CreateStars from '@/components/common/background/CreateStars';
 describe('CreateStars component', () => {
   it('renders without crashing and displays the star at random position', () => {
     const mockWindowSize = { width: 1920, height: 1080 };
-    const { container } = render(
-      <CreateStars i={1} windowSize={mockWindowSize} />
-    );
+    render(<CreateStars i={1} windowSize={mockWindowSize} />);
 
-    const starElement = container.querySelector('div');
-    expect(starElement).toBeInTheDocument();
+    const starElements = screen.getAllByTestId('star');
+    starElements.forEach((starElement) => {
+      expect(starElement).toBeInTheDocument();
+      expect(starElement).toHaveClass('absolute', 'rounded-full', 'bg-white');
 
-    expect(starElement).toHaveClass('absolute', 'rounded-full', 'bg-white');
-
-    const topPosition = parseInt(starElement!.style.top, 10);
-    const leftPosition = parseInt(starElement!.style.left, 10);
-    expect(topPosition).toBeGreaterThanOrEqual(0);
-    expect(topPosition).toBeLessThanOrEqual(mockWindowSize.height);
-    expect(leftPosition).toBeGreaterThanOrEqual(0);
-    expect(leftPosition).toBeLessThanOrEqual(mockWindowSize.width);
+      const topPosition = parseInt(starElement!.style.top, 10);
+      const leftPosition = parseInt(starElement!.style.left, 10);
+      expect(topPosition).toBeGreaterThanOrEqual(0);
+      expect(topPosition).toBeLessThanOrEqual(mockWindowSize.height);
+      expect(leftPosition).toBeGreaterThanOrEqual(0);
+      expect(leftPosition).toBeLessThanOrEqual(mockWindowSize.width);
+    });
   });
 });
