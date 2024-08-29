29dda4ff3bea11375150ad92c1230fa62b8239a5
j8won <joowoni99@gmail.com>
2024-08-29 13:54:38 +0900

    :zap: Memoize CreateStars component with React.memo to optimize perfomance and avoid unnecessary re-renders

--- src/components/common/background/CreateStars.tsx
+++ src/components/common/background/CreateStars.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { getRandomValue } from '@/utils/random';
 
 const STYLES = ['w-[4px] h-[4px]', 'w-[5px] h-[5px]', 'w-[6px] h-[6px]'];
@@ -31,4 +32,4 @@ function CreateStars({ i, windowSize }: CreateStarsProps) {
   );
 }
 
-export default CreateStars;
+export default memo(CreateStars);
